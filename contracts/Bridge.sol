08c9ec5c599d211ba395db6786219f14ccbcdb86
Bumble Bee <valuebumblebee@gmail.com>
2021-09-06 09:49:54 +0700

    Update events

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-IDentifier: MIT
 
 pragma solidity ^0.8.0;
 
@@ -9,23 +9,23 @@ contract Bridge is Ownable {
     IMintableBurnableToken public token;
     mapping(uint256 => mapping(bytes32 => bool)) processedChainTxns;
 
-    event Transfer(address from, address to, uint256 amount, uint256 toChainId);
-    event Mint(address from, address to, uint256 amount, uint256 fromChainId, bytes32 fromChainTxn);
+    event Transfer(address from, address to, uint256 amount, uint256 toChainID, uint256 timestamp);
+    event Mint(address from, address to, uint256 amount, uint256 fromChainID, bytes32 fromChainTxn, uint256 timestamp);
 
     constructor(address _token) {
         token = IMintableBurnableToken(_token);
     }
 
-    function transferTo(address to, uint256 amount, uint256 chainId) external {
-        require(chainId != block.chainid, "cannot bridge to the same chain");
+    function transferTo(address to, uint256 amount, uint256 toChainID) external {
+        require(toChainID != block.chainid, "cannot bridge to the same chain");
         token.burn(msg.sender, amount);
-        emit Transfer(msg.sender, to, amount, chainId);
+        emit Transfer(msg.sender, to, amount, toChainID, block.timestamp);
     }
 
     function mint(address to, uint256 amount, uint256 fromChainID, bytes32 fromChainTxn) external onlyOwner {
         require(processedChainTxns[fromChainID][fromChainTxn] == false, "transfer already processed");
         processedChainTxns[fromChainID][fromChainTxn] = true;
         token.mint(to, amount);
-        emit Mint(msg.sender, to, amount, fromChainID, fromChainTxn);
+        emit Mint(msg.sender, to, amount, fromChainID, fromChainTxn, block.timestamp);
     }
 }
